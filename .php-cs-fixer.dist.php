770ec588b18667a5d56ba1ff61c42c77256c4352
tam5 <arimiller92@gmail.com>
2021-09-13 11:08:57 -0400

    update

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,10 +2,7 @@
 
 $finder = Symfony\Component\Finder\Finder::create()
     ->in([
-        __DIR__ . '/app',
-        __DIR__ . '/config',
-        __DIR__ . '/database',
-        __DIR__ . '/routes',
+        __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
     ->name('*.php')
